614e4472dd643ce1c1d60197a8ab3f8229115255
samuel Adeyinka <tboysammy@gmail.com>
2024-08-06 01:10:38 +0100

    frontend

--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -2,4 +2,3 @@ import taskBlock from './1-block-scoped.js';
 
 console.log(taskBlock(true));  // Expected output: [ false, true ]
 console.log(taskBlock(false)); // Expected output: [ false, true ]
-
